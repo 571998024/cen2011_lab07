cbaee4294be39090936c76342d09f1e72127f149
Apiwat  Thaiyai <apiwat.tha@crru.ac.th>
2015-09-25 14:25:05 +0700

    update java file Array

--- Array01.java
+++ Array01.java
@@ -1,9 +1,16 @@
 public class Array01 {
   public static void main (String args[]) {
-      int[] a;
-      
-      // Assign Value to A
-      
-      // Print a Array
+		int [] a= new int [8];
+		a[0]=1;
+		a[1]=2;
+		a[2]=3;
+		a[3]=4;
+		a[4]=5;
+		a[5]=6;
+		a[6]=7;
+		a[7]=8;
+		System.out.println("A="+a);
+   
+	  // Print a Array
   }
 }
