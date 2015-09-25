3e0a631bc6f59f039dc905e688bfa321ba5e8fb5
Apiwat  Thaiyai <apiwat.tha@crru.ac.th>
2015-09-25 19:26:45 +0700

    update java flie Array

--- Array01.java
+++ Array01.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class Array01 {
   public static void main (String args[]) {
-	  
 	  Scanner input = new Scanner(System.in);
 	  System.out.print("Insert Your Number : ");
 	  int num =input.nextInt();
@@ -9,6 +8,5 @@ public class Array01 {
 	  for (int s = 1 ; s <= a.length ; s++){
 		  System.out.println("A = "+s);
 	  }
-      
   }
 }
