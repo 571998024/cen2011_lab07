54c3475c6f089adaf569654fab76a0967183da2b
Apiwat  Thaiyai <apiwat.tha@crru.ac.th>
2015-09-25 14:48:43 +0700

    update java file Array

--- Array01.java
+++ Array01.java
@@ -3,11 +3,11 @@ public class Array01 {
   public static void main (String args[]) {
 	  
 	  Scanner input = new Scanner(System.in);
-	  System.out.print("Insert Your Num : ");
+	  System.out.print("Insert Your Number : ");
 	  int num =input.nextInt();
       int[] a = new int[num];
-	  for (int i = 1 ; i <= a.length ; i++){
-		  System.out.println("A = "+i);
+	  for (int s = 1 ; s <= a.length ; s++){
+		  System.out.println("A = "+s);
 	  }
       
   }
